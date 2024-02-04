2816379109f11e564ddc879ad09af487af08547a
RazvanCretu <razvan.cretu97@gmail.com>
2024-02-04 22:00:07 +0000

    Refactor:
    Updates to stack so vpc is created and also a ec2 instance with bootstrap script

--- bin/dev-stack.js
+++ bin/dev-stack.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
-const cdk = require("aws-cdk-lib");
-const { Ec2BootupStack } = require("../lib/ec2-bootup-stack");
+import { App } from "aws-cdk-lib";
+import { DevStack } from "../lib/dev-stack-lib.js";
+import { config } from "dotenv";
 
-require("dotenv").config();
+config();
 
-const app = new cdk.App();
-new Ec2BootupStack(app, "Ec2BootupStack", {
+const app = new App();
+new DevStack(app, "DevStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-
+  stackName: "DevStack",
+  description: "A Stack where development takes place.",
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
