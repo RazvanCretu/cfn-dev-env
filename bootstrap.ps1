1724459d3bd061b4007ead0c61fbcf9e8513dfd3
RazvanCretu <razvan.cretu97@gmail.com>
2025-05-01 23:36:25 +0300

    fix: bootstrap script

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -12,9 +12,9 @@ try {
 
     # ========== VSCode ==========
 
-    $wc.DownloadFile("https://code.visualstudio.com/sha/download?build=stable&os=win32-x64", "$HOME\Downloads\vscode.exe")
-    Start-Process -FilePath "$HOME\Downloads\vscode.exe" -ArgumentList '/verysilent /mergetasks=!runcode,desktopicon' -Wait
-    Remove-Item "$HOME\Downloads\vscode.exe"
+    # $wc.DownloadFile("https://code.visualstudio.com/sha/download?build=stable&os=win32-x64", "$HOME\Downloads\vscode.exe")
+    # Start-Process -FilePath "$HOME\Downloads\vscode.exe" -ArgumentList '/verysilent /mergetasks=!runcode,desktopicon' -Wait
+    # Remove-Item "$HOME\Downloads\vscode.exe"
     
     # ========== AWSCLI ==========
 
@@ -32,12 +32,12 @@ try {
 
     $wc.DownloadFile("https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe", "$HOME\Downloads\miniconda3.exe")
     Start-Process -FilePath "$HOME\Downloads\miniconda3.exe" -ArgumentList '/S /InstallationType=JustMe /AddToPath=1 /RegisterPython=1 /D=$HOME\miniconda3' -Wait
-    Remove-Item "$HOME\Downloads\miniconda3.exe"
     Invoke-Expression "$HOME\miniconda3\condabin\conda init"
+    Remove-Item "$HOME\Downloads\miniconda3.exe"
 
     # ========== Git ==========
 
-    $wc.DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.45.2.windows.1/Git-2.45.2-64-bit.exe", "$HOME\Downloads\git.exe")
+    $wc.DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe", "$HOME\Downloads\git.exe")
     Start-Process -FilePath "$HOME\Downloads\git.exe" -ArgumentList '/SP- /VERYSILENT /SUPPRESSMSGBOXES /FORCECLOSEAPPLICATIONS /NORESTART /NOCANCEL /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext,ext\reg,ext\shellhere,ext\guihere,gitlfs,assoc,assoc_sh,autoupdate" /LOG="C:\git-for-windows.log"' -Wait 
     Remove-Item "$HOME\Downloads\git.exe"
     
@@ -47,11 +47,11 @@ try {
 
     # ========== RClone ==========
     
-    mkdir c:\rclone 
+    mkdir C:\rclone 
 
-    $wc.DownloadFile("https://downloads.rclone.org/v1.68.2/rclone-v1.68.2-windows-amd64.zip", "C:\rclone\rclone.zip")
-    Expand-Archive -path 'c:\rclone\rclone.zip' -destinationpath 'C:\rclone\'
-    Copy-Item "C:\rclone\rclone-v1.68.2-windows-amd64\*.*" "C:\rclone\"
+    $wc.DownloadFile("https://downloads.rclone.org/v1.69.1/rclone-v1.69.1-windows-amd64.zip", "C:\rclone\rclone.zip")
+    Expand-Archive -path 'C:\rclone\rclone.zip' -destinationpath 'C:\rclone\'
+    Copy-Item "C:\rclone\rclone-v1.69.1-windows-amd64\*.*" "C:\rclone\"
 
     [Environment]::SetEnvironmentVariable('Path', "$([Environment]::GetEnvironmentVariable('Path', 'User'));C:\rclone", 'User')
 
@@ -70,7 +70,7 @@ try {
     . $PROFILE
 
     Write-Host The following software was installed:
-    Write-Host VSCode (code --version)
+    # Write-Host VSCode (code --version)
     Write-Host (conda --version)
     Write-Host (python --version)
     Write-Host (pip --version)
@@ -78,33 +78,44 @@ try {
     Write-Host (rclone --version)
     Write-Host (aws --version)
 
-    rclone config create general s3 provider=AWS env_auth=true region=eu-central-1 location_contraint=EU server_side_encryption=AES256 storage_class=STANDARD
-
-    Start-Process powershell.exe { rclone mount general:general-cr3tu/ S: --vfs-cache-mode full --log-file='C:\rclone.log' } -Verb runas
+    Start-Process PowerShell -WindowStyle Hidden -ArgumentList "-Command & {
+        rclone config create general s3 provider=AWS env_auth=true region=eu-central-1 location_contraint=EU server_side_encryption=AES256 storage_class=STANDARD
+        rclone mount general:general-cr3tu/ S: --vfs-cache-mode full --log-file='C:\rclone.log'
+    }"
 
     if (!(Test-Path -Path $HOME\.projects)) {
         New-Item -ItemType Directory -Path $HOME\.projects -Force
     }
-
+    
     Set-Location $HOME\.projects
-
+    
     git clone "https://RazvanCretu:$env:GH_TOKEN@github.com/RazvanCretu/trader-bot.git"
-
+    
     Set-Location $HOME\.projects\trader-bot
-
+    
     git config --global user.name "RazvanCretu"
     git config --global user.email "razvan.cretu97@gmail.com"
-
+    
     python -m venv env
-
+    
     . ".\env\Scripts\activate.ps1"
 
     pip install -r requirements.txt
 
-    python src\main.py
+    $env:PYTHONBUFFERED = 1; python src\main.py
 
-    'start powershell "rclone mount general:general-cr3tu/ S: --vfs-cache-mode full --log-file=C:\rclone.log -v --no-console"' >> "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\mount-s3.cmd"
-    # 'start powershell.exe { "rclone mount general:general-cr3tu/ S: --vfs-cache-mode full --log-file=C:\rclone.log -v --no-console" }' >> "$Home\Desktop\mount-s3.cmd"
+    Set-Location "C:\Windows\System32\config\systemprofile\AppData\Local\Temp"
+    
+    $current_date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+    $logs = "S:\Trader Bot Logs\$current_date"
+    
+    New-Item -ItemType Directory -Path $logs
+    
+    Get-ChildItem . | Foreach-Object {
+        if ($_.Attributes -match "Directory") {
+            Copy-Item -Path "$($_.FullName)" -Destination $logs -Recurse
+        }
+    }
 
     [int]$ElapsedAll = $StopWatchAll.Elapsed.TotalSeconds
 
@@ -114,16 +125,6 @@ catch {
     "Error: $($Error[0])"
 }
 finally {
-    Set-Location "C:\Windows\System32\config\systemprofile\AppData\Local\Temp"
-
-    $current_date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
-    $logs = "S:\Trader Bot Logs\$current_date"
-
-    New-Item -ItemType Directory -Path $logs
-
-    Get-ChildItem . | Foreach-Object {
-        if ($_.Attributes -match "Directory") {
-            Copy-Item -Path "$($_.FullName)" -Destination $logs -Recurse
-        }
-    }
+    shutdown.exe /s
 }
+    
